a2c01f9fa36dfd9ed93234cf228be119f924493a
Absar69 <absarrashid3@gmail.com>
2024-04-24 10:30:42 +0500

    redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom';
+import { provider } from 'react-redux';
+import store from './store';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
@@ -29,7 +31,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <provider store={store}>
+      <RouterProvider router={router} />
+    </provider>
   </React.StrictMode>
 );
 
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -32,3 +32,5 @@ const HomeScreen = () => {
 }
 
 export default HomeScreen;
+
+
