8d80ad7a12a51b7dbecfff34de228ea874619c00
Absar69 <absarrashid3@gmail.com>
2024-04-24 11:13:00 +0500

    Product Api Slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider
-} from 'react-router-dom';
-import { provider } from 'react-redux';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux'; // Correct casing for Provider
 import store from './store';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
@@ -17,25 +11,22 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
-
   createRoutesFromElements(
     <Route Path='/' element={<App />} >
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
-
     </Route> 
-
-)
+  )
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <provider store={store}>
+    {/* Wrap your app with Provider */}
+    <Provider store={store}>
       <RouterProvider router={router} />
-    </provider>
+    </Provider>
   </React.StrictMode>
 );
-
 
 reportWebVitals();
