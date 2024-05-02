aa1745358c94bee10486a073a16171bdd576ece5
Absar69 <absarrashid3@gmail.com>
2024-05-03 02:05:36 +0500

    User Registration Endpoint and Encryption

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+const generateToken = (res, userId) => {
+
+    const token = jwt.sign({
+        userId
+        
+    }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    });
+
+    // set JWT token in cookie HTTP-ONLY
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        SameSite: 'strict',
+        maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+    });
+
+
+
+
+
+
+}
+
+export default generateToken;
